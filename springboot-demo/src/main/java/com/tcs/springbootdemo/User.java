fbee03bfc2dfbe51b940abfeb7d195f44a328177
amitkumar-29 <tcsamitkumar29@gmail.com>
2021-08-26 05:41:05 +0000

    Lombok library added

--- springboot-demo/src/main/java/com/tcs/springbootdemo/User.java
+++ springboot-demo/src/main/java/com/tcs/springbootdemo/User.java
@@ -6,20 +6,20 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
 
+import lombok.Getter;
+import lombok.Setter;
+
 @Entity
+@Setter
+@Getter
 public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	
+	@NotNull
 	private String firstName;
      
-	@NotNull
-	public String getFirstName() {
-		return firstName;
-	}
+	
 
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
 }
